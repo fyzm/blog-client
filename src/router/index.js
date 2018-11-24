c913d96aac6bc8bc6e3d0f060ff3eb8f840b394a
fyzm <fangyuan931018@gmail.com>
2018-11-25 03:06:44 +0800

    add some create and index

--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,24 @@ import Index from '@/pages/Index/template.vue'
 import My from '@/pages/My/template.vue'
 import Register from '@/pages/Register/template.vue'
 import User from '@/pages/User/template.vue'
+
+import store from '../store'
+
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
+    {
+      path: '/',
+      component: Index
+    },
     {
       path: '/login',
       component: Login
+    },
+    {
+      path: '/register',
+      component: Register
     },
     {
       path: '/detail/:blogId',
@@ -22,23 +33,18 @@ export default new Router({
     },
     {
       path: '/create',
-      component: Create
+      component: Create,
+      meta: { requiresAuth: true}
     },
     {
       path: '/edit/:blogId',
-      component: Edit
-    },
-    {
-      path: '/',
-      component: Index
+      component: Edit,
+      meta: { requiresAuth: true}
     },
     {
       path: '/my',
-      component: My
-    },
-    {
-      path: '/register',
-      component: Register
+      component: My,
+      meta: { requiresAuth: true}
     },
     {
       path: '/user/:userId',
@@ -46,3 +52,22 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    store.dispatch('checkLogin').then(isLogin=>{
+      if (!isLogin) {
+        next({
+          path: '/login',
+          query: { redirect: to.fullPath }
+        })
+      } else {
+        next()
+      }
+    })
+  } else {
+    next()
+  }
+})
+
+export default router
